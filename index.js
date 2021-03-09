507faa8441efa8a61f6738a0fdd36b55a3625d5e
ckkeliih <ckkeliih@hawaii.edu>
2021-03-09 01:15:31 +0000

    Updated code to receive GPS coordinates as input.

--- index.js
+++ index.js
@@ -15,13 +15,16 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res) {
     
     // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
+        var lat = String(req.body.latInput);
+        console.log(req.body.latInput);
+
+        var lon = String(req.body.lonInput);
+        console.log(req.body.lonInput);
     
     //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
         const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
+        const apiKey = "12f0882419161c36a8f0837248f8440f";
+        const url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat +  "&lon=" + lon + "&APPID=" + apiKey;
     
     // this gets the data from Open WeatherPI
     https.get(url, function(response){
@@ -31,14 +34,17 @@ app.post("/", function(req, res) {
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
             const city = weatherData.name;
+            const cloud = weatherData.clouds.all;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
             
             // displays the output of the results
             res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
+            res.write("<h2>The GPS coordinates for " + city + " is " + lat + "° " + lon + "°.<br>" + "<h2>The Temperature is " + temp + " Degrees Fahrenheit" + " with a " + humidity + "% humidity, a wind speed of " + windSpeed + " mph and cloudiness of " + cloud + "%.<h2>");
             res.write("<img src=" + imageURL +">");
             res.send();
         });
